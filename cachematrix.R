79a090be7d37f165365908dc1e6b30157f0d0822
lucita1981 <36177396+lucita1981@users.noreply.github.com>
2018-02-21 21:11:03 -0500

    Latest version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 - R Programming
+## Matrix inversion is usually a costly computation and there may be some benefit to 
+## caching the inverse of a matrix rather than compute it repeatedly 
+## This assignment is to write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## 1. makeCacheMatrix: This function creates a special "matrix" object that 
+## can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+      x <<- y
+      i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## 2. cacheSolve: This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated  
+## (and the matrix has not changed), then the cachesolve should retrieve the inverse 
+## from the cache. This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+## Sample:
+## > x <- matrix(2:5,2,2)     Creating matrix
+## > m <- makeCacheMatrix(x)  Creating special matrix
+## > m$get()                  Getting matrix
+## [,1] [,2]
+## [1,]    2    4
+## [2,]    3    5
+## > cacheSolve(m)            Calculating inverse matrix
+## [,1] [,2]
+## [1,] -2.5    2
+## [2,]  1.5   -1
+## > cacheSolve(m)            Calculating inverse matrix again
+## getting cached data        Verifying it got from cache
+## [,1] [,2]
+## [1,] -2.5    2
+## [2,]  1.5   -1
+
